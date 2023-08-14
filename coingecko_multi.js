1e170171e2ee971feeaf25ed011ce5c0fdef90f1
Talent0407 <imasnpsorrels5@gmail.com>
2023-08-14 15:30:33 +0800

    api

---
+++ coingecko_multi.js
@@ -0,0 +1,18 @@
+const CoinGeckoClient = new CoinGecko();
+    let data = await CoinGeckoClient.exchanges.fetchTickers('bitfinex', {
+        coin_ids: ['bitcoin', 'ethereum', 'ripple', 'litecoin', 'stellar']
+    });
+    var _coinList = {};
+    var _datacc = data.data.tickers.filter(t => t.target == 'USD');
+    [
+        'BTC',
+        'ETH',
+        'XRP',
+        'LTC',
+        'XLM'
+    ].forEach((i) => {
+        var _temp = _datacc.filter(t => t.base == i);
+        var _res = _temp.length == 0 ? [] : _temp[0];
+        _coinList[i] = _res.last;
+    })
+    console.log(_coinList);
